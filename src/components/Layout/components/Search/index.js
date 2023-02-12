369a8b5423f92545f31a2a9bbc3ff63e8c738c62
Tai272620 <90725230+Tai272620@users.noreply.github.com>
2023-02-12 09:42:42 +0900

    update

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -52,6 +52,17 @@ function Search() {
         setShowResult(false);
     };
 
+    const handleChange = (e) => {
+        const searchValue = e.target.value;
+        if (!searchValue.startsWith(' ')) {
+            setSearchValue(e.target.value);
+        }
+    };
+
+    const handleSubmit = () => {
+
+    }
+
     return (
         <HeadlessTippy
             interactive
@@ -74,7 +85,7 @@ function Search() {
                     value={searchValue}
                     placeholder="Search account and videos"
                     spellCheck={false}
-                    onChange={(e) => setSearchValue(e.target.value)}
+                    onChange={handleChange}
                     onFocus={() => setShowResult(true)}
                 />
                 {!!searchValue && !loading && (
@@ -84,7 +95,7 @@ function Search() {
                 )}
                 {loading && <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />}
 
-                <button className={cx('search-btn')}>
+                <button className={cx('search-btn')} onMouseDown={(e) => e.preventDefault()}>
                     <SearchIcon />
                 </button>
             </div>
